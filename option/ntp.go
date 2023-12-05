64d83725c9849fafe072193055db31efac22549d
世界 <i@sekai.icu>
2023-12-05 18:01:19 +0800

    Fix missing omitempty for NTP server fields

--- option/ntp.go
+++ option/ntp.go
@@ -2,8 +2,9 @@ package option
 
 type NTPOptions struct {
 	Enabled       bool     `json:"enabled"`
+	Server        string   `json:"server,omitempty"`
+	ServerPort    uint16   `json:"server_port,omitempty"`
 	Interval      Duration `json:"interval,omitempty"`
 	WriteToSystem bool     `json:"write_to_system,omitempty"`
-	ServerOptions
 	DialerOptions
 }
